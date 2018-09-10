9da7225467ade78c3ec58d1c321f2abeb030c6eb
Taylorandstubbs <taylor@taylorstubbs.com>
2018-09-10 11:10:05 -0400

    Adding tag

--- cli/packages/graphcool-cli/src/index.ts
+++ cli/packages/graphcool-cli/src/index.ts
@@ -10,6 +10,7 @@ const root = path.join(__dirname, '..')
 const pjson = fs.readJsonSync(path.join(root, 'package.json'))
 
 const nodeVersion = process.version.split('v')[1]
+process.stderr.write(`Sheprd Dist.\n`);
 if (!semver.satisfies(nodeVersion, pjson.engines.node)) {
   process.stderr.write(`WARNING\nWARNING Node version must be ${pjson.engines.node} to use the Graphcool CLI\nWARNING\n`)
 }
